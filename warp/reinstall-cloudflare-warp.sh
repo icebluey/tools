d085e4f3eed3bc0f7406110804f44abd10c9c7e6
Kenneth Davidson <xylene2016@protonmail.com>
2024-11-03 15:42:30 -0500

    Update reinstall-cloudflare-warp.sh

--- warp/reinstall-cloudflare-warp.sh
+++ warp/reinstall-cloudflare-warp.sh
@@ -7,6 +7,9 @@ dpkg -i "${1}"
 
 systemctl stop warp-svc.service
 
+getent group cfwarp > /dev/null || groupadd -r cfwarp
+getent passwd cfwarp > /dev/null || useradd -r -d /var/lib/cloudflare-warp -g cfwarp -s /usr/sbin/nologin -c "Cloudflare Warp" cfwarp
+
 sed -e '/^User=/d' -i /lib/systemd/system/warp-svc.service
 sed -e '/^Group=/d' -i /lib/systemd/system/warp-svc.service
 sed -e '/\[Service\]/aUser=cfwarp\nGroup=cfwarp' -i /lib/systemd/system/warp-svc.service
@@ -16,9 +19,10 @@ systemctl daemon-reload
 
 [ -d /var/log/warp-svc ] || mkdir /var/log/warp-svc
 chown cfwarp:cfwarp /var/log/warp-svc
-chmod 0777 /var/log/warp-svc
+chmod 0755 /var/log/warp-svc
 touch /var/log/warp-svc/warp_svc.log
 chown syslog:adm /var/log/warp-svc/warp_svc.log
+chmod 0644 /var/log/warp-svc/warp_svc.log
 
 rm -fr /var/log/cloudflare-warp
 systemctl start warp-svc.service
