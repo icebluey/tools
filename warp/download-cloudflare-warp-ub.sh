5095f3f98a5859a7a392f1219b059df100d0392a
Kenneth Davidson <xylene2016@protonmail.com>
2025-11-01 16:16:10 -0400

    Update download links in download-cloudflare-warp-ub.sh
    Added additional download links for Cloudflare Warp.

--- warp/download-cloudflare-warp-ub.sh
+++ warp/download-cloudflare-warp-ub.sh
@@ -35,6 +35,11 @@ cd "${_dir}"
 wget -c -t 9 -T 9 "https://pkg.cloudflareclient.com/${cloudflare_warp_filepath}"
 echo "Completed"
 exit
+#############################################################################
+
+https://pkg.cloudflareclient.com/pool/noble/main/c/cloudflare-warp/cloudflare-warp_2025.8.779.0_amd64.deb
+https://pkg.cloudflareclient.com/pool/jammy/main/c/cloudflare-warp/cloudflare-warp_2025.8.779.0_amd64.deb
+https://pkg.cloudflareclient.com/pool/focal/main/c/cloudflare-warp/cloudflare-warp_2025.8.779.0_amd64.deb
 
 https://pkg.cloudflareclient.com/pool/focal/main/c/cloudflare-warp/cloudflare-warp_2024.6.497-1_amd64.deb
 https://pkg.cloudflareclient.com/pool/focal/main/c/cloudflare-warp/cloudflare-warp_2024.11.150.0_amd64.deb
