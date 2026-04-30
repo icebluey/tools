596b1ead64f465f1d0dc2d692fe466d4a16c96d7
Kenneth Davidson <xylene2016@protonmail.com>
2026-04-29 20:14:43 -0400

    Update download-cloudflare-warp-ub.sh

--- warp/download-cloudflare-warp-ub.sh
+++ warp/download-cloudflare-warp-ub.sh
@@ -1,40 +1,71 @@
 #!/bin/bash
 set -e
+
+_dl_warp_ub() {
+  cloudflare_warp_ver=$(curl -s "https://pkg.cloudflareclient.com/dists/${_distro}/main/binary-amd64/Packages" \
+    | grep -E -o 'Version: [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' \
+    | head -n1 \
+    | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+  cloudflare_warp_filepath=$(curl -s "https://pkg.cloudflareclient.com/dists/${_distro}/main/binary-amd64/Packages" \
+    | grep -i 'Filename:' \
+    | head -n1 \
+    | awk -F: '{print $2}' | sed 's|[ \t]*||g')
+  printf "Downloading Cloudflare warp \033[32m${_dir} ${cloudflare_warp_ver}\033[0m:\n"
+  mkdir "${_dir}" && cd "${_dir}"
+  wget -c -t 9 -T 9 "https://pkg.cloudflareclient.com/${cloudflare_warp_filepath}"
+  cd ..
+  echo "Completed"
+}
+
+_dl_warp_el() {
+  cloudflare_warp_ver=$(curl -s "https://pkg.cloudflareclient.com/dists/${_distro}/main/binary-amd64/Packages" \
+    | grep -E -o 'Version: [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' \
+    | head -n1 \
+    | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+  printf "Downloading Cloudflare warp \033[32mel8/9 ${cloudflare_warp_ver}\033[0m:\n"
+  mkdir el8-9 && cd el8-9
+  wget -c -t 9 -T 9 "https://pkg.cloudflareclient.com/rpm/x86_64/cloudflare-warp-${cloudflare_warp_ver}.x86_64.rpm"
+  cd ..
+  echo "Completed"
+}
+
 case "${1}" in
   2004|20.04)
     _distro="focal"
     _dir="20.04"
+    _dl_warp_ub
     ;;
   2204|22.04)
     _distro="jammy"
     _dir="22.04"
+    _dl_warp_ub
     ;;
   2404|24.04)
     _distro="noble"
     _dir="24.04"
+    _dl_warp_ub
+    ;;
+  all)
+    _distro="focal"
+    _dir="20.04"
+    _dl_warp_ub
+    _distro="jammy"
+    _dir="22.04"
+    _dl_warp_ub
+    _distro="noble"
+    _dir="24.04"
+    _dl_warp_ub
+    _distro="jammy"
+    _dl_warp_el
     ;;
   *)
     _distro="jammy"
     _dir="22.04"
+    _dl_warp_ub
     ;;
 esac
-
-cloudflare_warp_ver=$(curl -s "https://pkg.cloudflareclient.com/dists/${_distro}/main/binary-amd64/Packages" \
-  | grep -E -o 'Version: [0-9]+\.[0-9]+\.[0-9]+' \
-  | head -n1 \
-  | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
-
-cloudflare_warp_filepath=$(curl -s "https://pkg.cloudflareclient.com/dists/${_distro}/main/binary-amd64/Packages" \
-  | grep -i 'Filename:' \
-  | head -n1 \
-  | awk -F: '{print $2}' | sed 's|[ \t]*||g')
-
-echo "Downloading Cloudflare warp ${cloudflare_warp_ver}:"
-mkdir "${_dir}"
-cd "${_dir}"
-wget -c -t 9 -T 9 "https://pkg.cloudflareclient.com/${cloudflare_warp_filepath}"
-echo "Completed"
 exit
+
 #############################################################################
 
 https://pkg.cloudflareclient.com/pool/noble/main/c/cloudflare-warp/cloudflare-warp_2025.8.779.0_amd64.deb
